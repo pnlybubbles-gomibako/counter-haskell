b4dee70ef34d9e4ac8bbbdf188aebadc2aac291b
pnlybubbles <pnlybubbles@gmail.com>
2017-02-06 21:58:20 +0900

    feat: add stack2

---
+++ stack2.hs
@@ -0,0 +1,30 @@
+-- Stack
+type Stack = [Integer]
+
+-- Stackの先頭に要素を追加
+-- (追加する要素) -> (初期Stack) -> (先端の要素, 更新されたStack)
+push :: Integer -> Stack -> (Integer, Stack)
+push c cs = (c, c:cs)
+
+-- Stackの専用の要素を取り出す
+-- (初期Stack) -> (取り出された要素, 更新されたStack)
+pop :: Stack -> (Integer, Stack)
+pop (c:cs) = (c, cs)
+
+-- 先頭から2つ取り出して2項演算を行い、先頭に結果を追加する
+--      (2項演算子                     ) -> Stack -> (Integer, Stack)
+calc :: (Integer -> Integer -> Integer) -> Stack -> (Integer, Stack)
+calc op stack = (i3, stack3)
+  where
+    (i1, stack1) = pop stack
+    (i2, stack2) = pop stack1
+    (i3, stack3) = push (op i1 i2) stack2
+
+ml = calc (*)
+ad = calc (+)
+sb = calc (-)
+dv = calc div
+
+main = do
+  let stack = [1, 2, 3, 4, 5]
+  print $ ad stack
